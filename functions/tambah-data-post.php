266cfa83fd63da9a4b33c97732bba3f72fcca8c1
andrenata <andrenata25@gmail.com>
2019-10-30 18:28:23 +0800

    Comment for Insert Post

--- functions/tambah-data-post.php
+++ functions/tambah-data-post.php
@@ -8,23 +8,29 @@
         $description = $_POST['webeditor'];
         $datenow = date(Y-m-d);
 
+        // AMBIL NAMA PHOTO
         $name = $_FILES['image']['name'];
         $file = $_FILES['image']['tmp_name'];
 
+        // UPLOAD PHOTO
         $upload = move_uploaded_file($file, "../images/$name");
 
         if ($upload) {
+                // SIMPAN KE DATABASE
                 $query = mysqli_query($koneksi, "INSERT INTO 
                 post(post_title, post_description, post_image, post_date, post_status)
                 VALUES('$title', '$description', '$name', '$datenow', '$status')");
 
                 if($query) {
+                        // JIKA BERHASIL DISIMPAN
                         header("location:../index.php");
                 }else{
+                        // JIKA GAGAL DISIMPAN
                         echo "Please try again!!!";
                 }
 
         }else{
+                // GAGAL UPLOAD PHOTO
                 echo "Please try again!!!";     
         }
    
